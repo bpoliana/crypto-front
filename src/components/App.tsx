37d833b2bfa43e1c67e97b61ff1bbd98a6d40553
Barbara Jaber <bpolianajaber@gmail.com>
2021-02-21 17:16:06 -0300

    feat: add axios call to backend and create router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import axios from 'axios'
 import { ThemeProvider } from '@material-ui/core'
 import { appTheme } from './theme'
 import { Login } from './Login/Login'
+import { CurrenciesView } from './CurrenciesView/CurrenciesView'
 
-const App = () => {
-  return (
-    <ThemeProvider theme={appTheme}>
-      <Login />
-    </ThemeProvider>
-  )
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { apiResponse: '' }
+  }
+
+  callAPI () {
+    const cryptoApiUrl = 'http://localhost:3000/'
+    try {
+      axios.get(cryptoApiUrl)
+        .then(res => this.setState({ apiResponse: res }))
+        .catch(err => err)
+    } catch (err) {
+      alert(err)
+    }
+  }
+
+  componentDidMount () {
+    this.callAPI()
+  }
+
+  render () {
+    return (
+      <ThemeProvider theme={appTheme}>
+        <BrowserRouter>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/api/login" component={Login} />
+          <Route exact path="/home" component={CurrenciesView} />
+        </BrowserRouter>
+      </ThemeProvider>
+    )
+  }
 }
 
 export default App
