e79ace16546c9b3ef2166702c52d592fd8d39093
Barbara Jaber <bpolianajaber@gmail.com>
2021-02-21 17:15:26 -0300

    chore: refactor webpack config file

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,36 +1,38 @@
-const path = require("path");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+import path from 'path'
+import HtmlWebPackPlugin from 'html-webpack-plugin'
 
 // Instantiate the plugin.
 // The `template` property defines the source
 // of a template file that this plugin will use.
 // We will create it later.
 const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./src/index.html",
-});
+  template: './src/index.html'
+})
 
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: './src/index.tsx',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js']
   },
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
   plugins: [htmlPlugin],
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
+    historyApiFallback: true,
     port: 9000
   }
-};
+}
