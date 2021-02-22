9bca706b23fd2f61838e58e96ac393c6ba6abbbc
Barbara Jaber <bpolianajaber@gmail.com>
2021-02-21 21:34:36 -0300

    feat: ensure currency value changes on update-button click

--- src/client/client.tsx
+++ src/client/client.tsx
@@ -16,7 +16,27 @@ export const getCurrencies = async () => {
       'Content-Type': 'application/json',
       Authorization: localStorage.getItem('token')
     }
-    return await axios.get(`${cryptoApiUrl}/api/crypto/btc`, { headers: header }).then(res => res.data).catch(err => err)
+    const response = await axios.get(`${cryptoApiUrl}/api/crypto/btc`, { headers: header }).catch(err => err)
+    localStorage.setItem('token', response.config.headers.Authorization)
+    return response.data
+  } catch (err) {
+    alert(err)
+  }
+}
+
+export const postCurrency = async (currency, value) => {
+  try {
+    const header = {
+      'Content-Type': 'application/json',
+      Authorization: localStorage.getItem('token')
+    }
+    const body = {
+      currency: currency,
+      value: value
+    }
+    const response = await axios.post(`${cryptoApiUrl}/api/crypto/btc`, body, { headers: header }).catch(err => err)
+    localStorage.setItem('token', response.config.headers.Authorization)
+    return response.data
   } catch (err) {
     alert(err)
   }
--- src/components/UpdateCurrency/UpdateCurrency.tsx
+++ src/components/UpdateCurrency/UpdateCurrency.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import InputLabel from '@material-ui/core/InputLabel'
 import FormHelperText from '@material-ui/core/FormHelperText'
@@ -6,25 +6,59 @@ import FormControl from '@material-ui/core/FormControl'
 import NativeSelect from '@material-ui/core/NativeSelect'
 import { Button, Card, Container, Grid, TextField } from '@material-ui/core'
 import { useStyles } from './UpdateCurrency.styles'
+import { getCurrencies, postCurrency } from '../../client/client'
 
 export default function NativeSelects () {
   const classes = useStyles()
-  const [state, setState] = React.useState<{ value: string | number, currency: string }>({
-    value: '',
-    currency: 'hai'
+  const history = useHistory()
+
+  const [currencySelected, setCurrencySelected] = useState('BRL')
+  const [currencyValue, setCurrencyValue] = useState(0)
+
+  const [currencies, setCurrencies] = useState({
+    BRL: '0',
+    CAD: '0',
+    EUR: '0'
   })
 
-  const handleChange = (event: React.ChangeEvent<{ name?: string, value: unknown }>) => {
-    const name = event.target.name as keyof typeof state
-    setState({
-      ...state,
-      [name]: event.target.value
-    })
-  }
-  const history = useHistory()
+
+  useEffect(() => {
+    async function setApiCurrencies () {
+      const currenciesResponse = await getCurrencies()
+      setCurrencies({
+        BRL: currenciesResponse.bpi.BRL.rate,
+        CAD: currenciesResponse.bpi.CAD.rate,
+        EUR: currenciesResponse.bpi.EUR.rate
+      })
+    }
+    setApiCurrencies()
+  }, [])
 
   const redirect = () => {
     history.push('/home')
+  }
+
+  const submit = async () => {
+    await postCurrency(currencySelected, currencyValue)
+    const currenciesResponse = await getCurrencies()
+    setCurrencies({
+      BRL: currenciesResponse.bpi.BRL.rate,
+      CAD: currenciesResponse.bpi.CAD.rate,
+      EUR: currenciesResponse.bpi.EUR.rate
+    })
+  }
+
+  const getSelectedCurrencyValue = () => {
+    switch (currencySelected) {
+      case 'BRL':
+        return currencies.BRL
+      case 'EUR':
+        return currencies.EUR
+      case 'CAD':
+        return currencies.CAD
+      default:
+        return 0
+    }
   }
 
   return (
@@ -41,8 +75,8 @@ export default function NativeSelects () {
               Moeda
             </InputLabel>
             <NativeSelect
-              value={state.currency}
-              onChange={handleChange}
+              value={currencySelected}
+              onChange={(e) => { setCurrencySelected(e.target.value) }}
               inputProps={{
                 name: 'currency',
                 id: 'currency-native-label-placeholder'
@@ -52,9 +86,9 @@ export default function NativeSelects () {
               <option value={'EUR'}>EUR</option>
               <option value={'CAD'}>CAD</option>
             </NativeSelect>
-            <FormHelperText className={classes.formHelperText}>Valor atual: R$5,40</FormHelperText>
-            <TextField required id="outlined-basic" variant="outlined" label="Novo Valor" defaultValue="" />
-            <Button className={classes.buttonWrapper} color='primary' variant='contained'> Atualizar </Button>
+            <FormHelperText className={classes.formHelperText}>Valor atual: {getSelectedCurrencyValue()} </FormHelperText>
+            <TextField required id="outlined-basic" variant="outlined" label="Novo Valor" type="number" onChange={(e) => { setCurrencyValue(parseInt(e.target.value)) }} defaultValue="" />
+            <Button className={classes.buttonWrapper} color='primary' variant='contained' onClick={ async () => { submit() }} > Atualizar </Button>
           </FormControl>
         </Card>
       </form>
